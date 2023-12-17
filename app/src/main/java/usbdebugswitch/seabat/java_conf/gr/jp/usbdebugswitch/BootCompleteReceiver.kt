865de7c3c6dbf71af4974c8afc63da4428f49d0e
seabat <ryouta.nakasnishi.86@gmail.com>
2023-12-17 11:51:45 +0900

    BroadcastReceiver の中で coroutine を使用する

--- app/src/main/java/usbdebugswitch/seabat/java_conf/gr/jp/usbdebugswitch/BootCompleteReceiver.kt
+++ app/src/main/java/usbdebugswitch/seabat/java_conf/gr/jp/usbdebugswitch/BootCompleteReceiver.kt
@@ -6,6 +6,11 @@ import android.content.Intent
 import android.os.Build
 import android.preference.PreferenceManager
 import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import usbdebugswitch.seabat.java_conf.gr.jp.usbdebugswitch.utils.OverlayPermissionChecker
 
 class BootCompleteReceiver : BroadcastReceiver() {
@@ -15,18 +20,11 @@ class BootCompleteReceiver : BroadcastReceiver() {
         private val DEBUG = BuildConfig.DEBUG
     }
 
+    private val receiverScope = CoroutineScope(Dispatchers.Main + Job())
+
     override fun onReceive(context: Context, intent: Intent) {
         if(DEBUG) Log.d(TAG, "START RECEIVER")
         if(!Intent.ACTION_BOOT_COMPLETED.equals(intent.action)) {
-            return
-        }
-
-        val OFF = context.getString(R.string.setting_overlay_off)
-
-        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
-        val statusString = sharedPref.getString("pref_setting_overlay", OFF)
-        if(DEBUG) Log.d(TAG, "overlay status = ${statusString}")
-        if (OFF == statusString) {
             return
         }
 
@@ -34,13 +32,32 @@ class BootCompleteReceiver : BroadcastReceiver() {
             return
         }
 
-        if(DEBUG) Log.d(TAG, "start OverlayService")
-        val intent = Intent(context, OverlayService::class.java)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            context.startForegroundService(intent);
-              // Android O からはバックグラウンド状態のアプリからバックグラウンドサービスを起動できない
-        } else {
-            context.startService(intent);
+        val pendingResult = goAsync()
+
+        receiverScope.launch {
+            try {
+                withContext(Dispatchers.IO) {
+                    val offString = context.getString(R.string.setting_overlay_off)
+
+                    val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+                    val statusString = sharedPref.getString("pref_setting_overlay", offString)
+                    if(DEBUG) Log.d(TAG, "overlay status = $statusString")
+                    if (offString == statusString) {
+                        return@withContext
+                    }
+
+                    if(DEBUG) Log.d(TAG, "start OverlayService")
+                    val intent = Intent(context, OverlayService::class.java)
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        context.startForegroundService(intent)
+                        // Android O からはバックグラウンド状態のアプリからバックグラウンドサービスを起動できない
+                    } else {
+                        context.startService(intent);
+                    }
+                }
+            } finally {
+                pendingResult.finish()
+            }
         }
     }
 }
