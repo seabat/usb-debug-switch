646635f1310e63a5021f72e4a1a67a8da0ec985c
seabat <ryouta.nakasnishi.86@gmail.com>
2024-01-06 22:13:10 +0900

    設定画面のベースとヘッダーを作成

---
+++ app/src/main/java/dev/seabat/android/usbdebugswitch/pages/SettingFragment.kt
@@ -0,0 +1,32 @@
+package dev.seabat.android.usbdebugswitch.pages
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
+import dev.seabat.android.usbdebugswitch.compose.setting.SettingScreen
+import kotlinx.coroutines.launch
+
+class SettingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = ComposeView(requireContext()).apply {
+            setContent {
+                SettingScreen {
+                    lifecycleScope.launch {
+                        findNavController().popBackStack()
+                    }
+                }
+            }
+        }
+        return view
+    }
+}
