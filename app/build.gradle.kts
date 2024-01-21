7db037acdf4a02210b3d0522592dd53c3adbb180
seabat <ryouta.nakasnishi.86@gmail.com>
2024-01-21 16:28:39 +0900

    1.0.0(7) -> 1.1.0(8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.seabat.android.usbdebugswitch"
         minSdk = 26
         targetSdk = 33
-        versionCode = 7
-        versionName = "1.0.0"
+        versionCode = 8
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
