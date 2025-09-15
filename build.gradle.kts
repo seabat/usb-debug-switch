00147785a4bb6dc3d84ee3bf24d898159beffb7c
seabat <ryouta.nakasnishi.86@gmail.com>
2025-09-15 11:56:31 +0900

    Kotlin 1.9.0 -> 2.2.0、そのほかライブラリのバージョンアップ

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,20 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-val kotlinVersion: String by extra("1.9.0")
+val kotlinVersion: String by extra("2.2.0")
 
 buildscript {
     repositories {
         google()
     }
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
-        classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.9.4")
+        classpath("com.google.android.gms:oss-licenses-plugin:0.10.8")
     }
 }
 
 plugins {
     id("com.android.application") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
 }
